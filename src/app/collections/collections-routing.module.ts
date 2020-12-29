5b799f8a03440de6a4cb58c6f22aeaf8b024f427
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-12-29 11:06:55 -0800

    Added collection-home and routes to app.component.

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CollectionsHomeComponent} from './collections-home/collections-home.component'
 
-const routes: Routes = [];
+//if user is at path /collections show the CollectionsHomeComponent
+const routes: Routes = [  { path: 'collections', component: CollectionsHomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
