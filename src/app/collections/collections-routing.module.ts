250067fc4f465333f525cff4955fa1af06d6fc72
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-12-30 13:05:02 -0800

    Added Relative RouterLink references

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CollectionsHomeComponent} from './collections-home/collections-home.component'
-const routes: Routes = [{ path: '', component: CollectionsHomeComponent }]; //if user is at path /collections show the CollectionsHomeComponent
+import { CollectionsHomeComponent} from './collections-home/collections-home.component';
+import { PartnersComponent } from './partners/partners.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { BiographyComponent } from './biography/biography.component';
+
+//if user is at path /collections show the CollectionsHomeComponent
+const routes: Routes = [
+  { path: '', component: CollectionsHomeComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnersComponent }
+    ]
+ }
+]; 
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
