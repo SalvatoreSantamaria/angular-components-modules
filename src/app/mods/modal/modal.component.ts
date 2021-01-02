208667ca59f3e8c9f604ffd06af748c5a38c095c
Salvatore Santamaria <sam.santamaria@gmail.com>
2021-01-01 19:16:58 -0800

    Fixed modal

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { componentFactoryName } from '@angular/compiler';
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, Output, EventEmitter } from '@angular/core';
 
 // ElementRef gives us access to the host element of the component
 
@@ -10,6 +10,9 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
+  //event we are watching for on the parent component- it is an intance for new EventEmitter
+  @Output() close = new EventEmitter
+  
   constructor(private el: ElementRef) {
     console.log(this.el.nativeElement)
    }
@@ -22,6 +25,13 @@ export class ModalComponent implements OnInit {
   // this removes the modal after navigating away from the modal.
   ngOnDestroy() {
     this.el.nativeElement.remove()
+  }
+
+  onCloseClick() {
+    //this also works (i just jammed this in here to see if it would)
+    // document.body.removeChild(this.el.nativeElement)
+
+    this.close.emit();
   }
 
 }
